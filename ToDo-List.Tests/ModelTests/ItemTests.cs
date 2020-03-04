e7ef0851d903046b5016174de2ab49edc41db394
Keturah Howard <keturah.dev@gmail.com>
2020-03-03 19:23:41 -0800

    create new test, unofficially fails because constructor is missing

--- ToDo-List.Tests/ModelTests/ItemTests.cs
+++ ToDo-List.Tests/ModelTests/ItemTests.cs
@@ -8,10 +8,21 @@ namespace ToDoList.Tests
   {
 
     [TestMethod]
-    public void ItemComstructor_CreatesInstanceOfItem_Item()
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
       Item newItem = new Item();
       Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      //Arange
+      string description = "Mow the lawn";
+      Item newItem = new Item(description);
+      //Act
+      string result = newItem.Description();
+      //Assert
+      Assert.AreEqual(result, description);
     }
   }
 }
