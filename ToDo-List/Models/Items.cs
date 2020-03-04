c3db63e9947a3d3c47706335daf90e1ebe842a84
Keturah Howard <keturah.dev@gmail.com>
2020-03-03 19:53:22 -0800

    create static variable and method for 4th spec - not-tested yet

--- ToDo-List/Models/Items.cs
+++ ToDo-List/Models/Items.cs
@@ -1,12 +1,21 @@
+using System.Collections.Generic;
+
 namespace ToDoList.Models
 {
   public class Item
   {
     public string Description {get; set;}
+    private static List<Item> _instances = new List<Item> {}; //new static variable
 
     public Item(string description)
     {
       Description = description;
+      _instances.Add(this); //pushes 'this' to the list automatically on creation!
+    }
+
+    public static List<Item> GetAll()
+    {
+      return _instances;
     }
   }
 }
