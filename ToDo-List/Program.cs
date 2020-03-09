d3e4fa03109d1c47aa5154d8ef715d408884add5
Keturah Howard and Kristina Hengster Tintor <keturah+kristina@>
2020-03-09 15:29:45 -0700

    create entrypoint for website building

---
+++ ToDo-List/Program.cs
@@ -0,0 +1,21 @@
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+// both Startup.cs and Program.cs files contain code in the same FriendLetter namespace. This ensures the code blocks in Startup.cs and Program.cs have access to each other.
+namespace MadLibs
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+
+      host.Run();
+    }
+  }
+}
